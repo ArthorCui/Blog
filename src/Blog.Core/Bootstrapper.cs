5ba3b4664b3b837a4e60d60ed812d08db55bfa85
arthor_cui <arthor.cui@gmail.com>
2014-08-28 07:58:59 +0800

    create blog solution and core library

---
+++ src/Blog.Core/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core
+{
+    public class Bootstrapper
+    {
+        private static bool _hasBeenIntialized = false;
+        internal const string CURRENT_ASSEMBLY = "Blog.Core";
+
+        public static void Start()
+        {
+            if (!_hasBeenIntialized)
+            {
+                _hasBeenIntialized = true;
+
+            }
+        }
+    }
+}
