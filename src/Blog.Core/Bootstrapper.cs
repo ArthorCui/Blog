c92a46f35c53897970449da483be94bc0fba1ec4
arthor_cui <arthor.cui@gmail.com>
2014-08-28 18:50:16 +0800

    remove framework subsonic unnecessary test and add blog core registry
    the lib use framework

--- src/Blog.Core/Bootstrapper.cs
+++ src/Blog.Core/Bootstrapper.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using OnePiece.Framework.Core;
+using StructureMap;
+using StructureMap.Configuration.DSL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +19,87 @@ namespace Blog.Core
         {
             if (!_hasBeenIntialized)
             {
+                Register(CURRENT_ASSEMBLY);
                 _hasBeenIntialized = true;
-
             }
         }
+
+        #region Registry
+
+        private static void Register(string assemblyName)
+        {
+            if (SingletonBase<IocFacotry>.Instance.Container == null)
+            {
+                SingletonBase<IocFacotry>.Instance.Container = new StructureMapContainer();
+            }
+            RegisterDIRegistry(assemblyName);
+            SingletonBase<BootHeart>.Instance.Boot(assemblyName);
+            RegisterMapper(assemblyName);
+        }
+
+        public static void RegisterBootable(Func<Assembly> getAssembly)
+        {
+            if (getAssembly != null)
+            {
+                Assembly assembly = getAssembly();
+                SingletonBase<BootHeart>.Instance.Boot(assembly.FullName);
+            }
+        }
+
+        private static void RegisterDIRegistry(Func<Assembly> getAssembly)
+        {
+            if (getAssembly != null)
+            {
+                List<Type> source = (from x in getAssembly().GetTypes()
+                                     where x.IsSubclassOf(typeof(BasicRegistry)) && !x.IsAbstract
+                                     select x).ToList<Type>();
+                if (source.Any<Type>())
+                {
+                    source.ForEach(delegate(Type t)
+                    {
+                        Action<ConfigurationExpression> configure = null;
+                        Registry instance = Activator.CreateInstance(t) as Registry;
+                        if (instance != null)
+                        {
+                            if (configure == null)
+                            {
+                                configure = x => x.AddRegistry(instance);
+                            }
+                            ObjectFactory.Configure(configure);
+                        }
+                    });
+                }
+            }
+        }
+
+        internal static void RegisterDIRegistry(string assemblyName)
+        {
+            List<Type> source = (from x in Assembly.Load(assemblyName).GetTypes()
+                                 where x.IsSubclassOf(typeof(BasicRegistry)) && !x.IsAbstract
+                                 select x).ToList<Type>();
+            if (source.Any<Type>())
+            {
+                source.ForEach(delegate(Type t)
+                {
+                    Action<ConfigurationExpression> configure = null;
+                    Registry instance = Activator.CreateInstance(t) as Registry;
+                    if (instance != null)
+                    {
+                        if (configure == null)
+                        {
+                            configure = x => x.AddRegistry(instance);
+                        }
+                        ObjectFactory.Configure(configure);
+                    }
+                });
+            }
+        }
+
+        internal static void RegisterMapper(string assembly)
+        {
+            SingletonBase<EntityMapping>.Instance.Register(assembly);
+        }
+
+        #endregion
     }
 }
